c8275a2a421277ca6ecff534a8bec020f1d52e9d
LeeBoSeul <95214882+boseuly@users.noreply.github.com>
2026-02-23 22:08:12 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
@@ -1,19 +1,10 @@
 import sys
-t = int(sys.stdin.readline().strip())
 result = ""
-def mod_num(dividend, divisor):    # dividend: 피제수(나뉨수), divisor: 제수(나눔수)
-    # 몫        나머지
-    quotient, dividend = divmod(dividend, divisor)   # 나누고 남은 값을 전달해준다. 
-    return quotient, dividend
-    
-
 divisors = [25, 10, 5, 1]   # 1센트 = 100달러 
-for _ in range(t):
+for _ in range(int(input())):
     return_money = int(sys.stdin.readline().strip())
     for divisor in divisors:
-        quotient, return_money = mod_num(return_money, divisor)
+        quotient, return_money = divmod(return_money, divisor)
         result += str(quotient) + " "
-        
     result += "\n"
 print(result)
-    
